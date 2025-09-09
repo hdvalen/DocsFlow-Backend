f310fd06e8b3d6cf7e09d784c56d0cd282a2460f
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-09 14:22:12 -0500

    feat: :sparkles: Se crearon los endpoints de documentos

--- app/main.py
+++ app/main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 from sqlmodel import Session
 from sqlalchemy import text
 from app.database.database import engine
-
 from app.routes.users.UsersRoutes import router as users_router
-
+from app.routes.documentPdf.DocumentRouter import router as document_router
 app = FastAPI()
-
-
 
 @app.on_event("startup")
 def test_db():
@@ -15,13 +12,15 @@ def test_db():
         with Session(engine) as session:
             session.exec(text("SELECT 1")) 
         print("Conexión a la base de datos exitosa")
+
     except Exception as e:
         print("❌ Error al conectar:", e)
 
 # Routers
 app.include_router(users_router, prefix="/users", tags=["Users"])
-
-
+app.include_router(document_router, prefix="/documents", tags=["Documents"])
+app.include_router(document_router, prefix="/documents", tags=["Documents"])
+app.include_router(document_router, prefix="/documents", tags=["Documents"])
 # Ruta raíz 
 @app.get("/")
 def root():

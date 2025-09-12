4e686f66735af7d5be285442b8194b740b52811a
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-11 19:34:38 -0500

    feat: :zap: Se agrego el endpoint de eliminar documento

--- app/routes/documentPdf/DocumentRouter.py
+++ app/routes/documentPdf/DocumentRouter.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, UploadFile, File, Form
 from sqlmodel import Session
 from app.database.database import get_db
-from app.controllers.documents.documentsController import guardar_documento, get_documents_by_user, get_documents_by_id
+from app.controllers.documents.documentsController import guardar_documento, get_documents_by_user, get_documents_by_id, get_all_documents, delete_document
 from app.models.Document.DocumentModel import DocumentCreate
 
 router = APIRouter(tags=["Documents"])
@@ -38,3 +38,13 @@ def read_documents_by_user(user_id: int, db: Session = Depends(get_db)):
 def read_document_by_id(document_id: int, db: Session = Depends(get_db)):
     document = get_documents_by_id(document_id=document_id, db=db)
     return {"documento": document}
+
+@router.get("/")
+def read_all_documents(db: Session = Depends(get_db)):
+    documents = get_all_documents(db=db)
+    return {"documents": documents}
+
+@router.delete("/{document_id}")
+def delete_document_route(document_id: int, db: Session = Depends(get_db)):
+    result = delete_document(document_id=document_id, db=db)
+    return result

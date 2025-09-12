abd10f002deb14e995f073946c72ef74ac462761
techforge25 <dayanahdz080506@gmail.com>
2025-09-12 07:34:51 -0500

    fix: corrected method name for user registration and updated CORS origins

--- app/main.py
+++ app/main.py
@@ -14,16 +14,18 @@ app = FastAPI()
 
 # 🔹 Configuración de CORS
 origins = [
-    "http://localhost:5174",   # Frontend en Vite
-    "http://127.0.0.1:5174",   # A veces Vite usa esta IP
+    "http://localhost:5173",  
+    "http://localhost:5174",  
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,       # dominios permitidos
+    allow_origins=origins,   
     allow_credentials=True,
-    allow_methods=["*"],         # todos los métodos HTTP
-    allow_headers=["*"],         # todos los headers
+    allow_methods=["*"],     
+    allow_headers=["*"],     
 )
 
 @app.on_event("startup")
